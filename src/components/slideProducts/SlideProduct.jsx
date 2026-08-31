308c5ab69ffefc006c18354a96e3a40a235ddc3d
Ahmed Khaled <ahmed.khaled2011k@gmail.com>
2026-09-01 00:01:02 +0300

    feat: add product slider component and define hero section styles

--- src/components/slideProducts/SlideProduct.jsx
+++ src/components/slideProducts/SlideProduct.jsx
@@ -1,6 +1,16 @@
 import Product from './Product'
 import './slideProduct.css'
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Autoplay, Navigation } from 'swiper/modules';
+
 function SlideProduct() {
     return (
         <div>
@@ -11,7 +21,37 @@ function SlideProduct() {
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, voluptates?</p>
                     </div>
 
-                    <Product />
+                    <Swiper
+                        loop={true}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+                        slidesPerView={5}
+                        spaceBetween={20}
+                        navigation={true}
+                        modules={[Autoplay,Navigation]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                        <SwiperSlide><Product /></SwiperSlide>
+                    </Swiper>
                 </div>
             </div>
         </div>
